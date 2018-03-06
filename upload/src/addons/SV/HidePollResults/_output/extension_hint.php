8b62b4892fa03016e16cabff7c8ef6f7a7893456
Xon <tazipper1@hotmail.com>
2018-03-06 17:32:53 +0800

    Remove dead code

--- upload/src/addons/SV/HidePollResults/_output/extension_hint.php
+++ upload/src/addons/SV/HidePollResults/_output/extension_hint.php
@@ -12,9 +12,3 @@ namespace SV\HidePollResults\XF\Entity
 {
 	class XFCP_Poll extends \XF\Entity\Poll {}
 }
-
-namespace SV\HidePollResults\XF\Pub\Controller
-{
-	class XFCP_Forum extends \XF\Pub\Controller\Forum {}
-	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
-}
