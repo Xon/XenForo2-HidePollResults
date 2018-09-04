4bdd84dcf70ad946ce5eff6914778bd11e5af978
Xon <tazipper1@hotmail.com>
2018-09-05 01:03:48 +0800

    Use shared installer code

--- upload/src/addons/SV/HidePollResults/Setup.php
+++ upload/src/addons/SV/HidePollResults/Setup.php
@@ -2,31 +2,40 @@
 
 namespace SV\HidePollResults;
 
+use SV\Utils\InstallerHelper;
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
 use XF\Db\Schema\Alter;
-use XF\Db\Schema\Create;
 
 class Setup extends AbstractSetup
 {
-	use StepRunnerInstallTrait;
-	use StepRunnerUpgradeTrait;
-	use StepRunnerUninstallTrait;
+    // from https://github.com/Xon/XenForo2-Utils cloned to src/addons/SV/Utils
+    use InstallerHelper;
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
 
-	public function installStep1() {
-		$this->schemaManager()->alterTable('xf_poll', function(Alter $table) {
-			$table->addColumn('hide_results')->type('tinyint')->length(3)->setDefault(0);
-			$table->addColumn('until_close')->type('tinyint')->length(3)->setDefault(0);
-		});
+    public function installStep1()
+    {
+        $this->schemaManager()->alterTable('xf_poll', function (Alter $table) {
+            $this->addOrChangeColumn($table, 'hide_results')->type('tinyint')->length(3)->setDefault(0);
+            $this->addOrChangeColumn($table, 'until_close')->type('tinyint')->length(3)->setDefault(0);
+        });
 
-		\XF::app()->simpleCache()->setValue($this->addOn->getAddOnId(), 'HidePollResults', false);
-	}
+        \XF::app()->simpleCache()->setValue($this->addOn->getAddOnId(), 'HidePollResults', false);
+    }
 
-	public function uninstallStep1(){
-		$this->schemaManager()->alterTable('xf_poll', function(Alter $table) {
-			$table->dropColumns(['hide_results', 'until_close']);
-		});
-	}
+    public function upgrade2000200Step1()
+    {
+        $this->installStep1();
+    }
+
+    public function uninstallStep1()
+    {
+        $this->schemaManager()->alterTable('xf_poll', function (Alter $table) {
+            $table->dropColumns(['hide_results', 'until_close']);
+        });
+    }
 }
