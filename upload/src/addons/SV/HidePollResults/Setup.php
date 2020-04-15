c69c6e9d0f42300cbe6c5e137789336d71318d8a
Xon <tazipper1@hotmail.com>
2020-04-15 10:53:01 +0800

    Rework installer to be more standard & rename phrases & template

--- upload/src/addons/SV/HidePollResults/Setup.php
+++ upload/src/addons/SV/HidePollResults/Setup.php
@@ -19,34 +19,93 @@ class Setup extends AbstractSetup
 
     public function installStep1()
     {
-        $this->schemaManager()->alterTable('xf_poll', function (Alter $table) {
-            $this->addOrChangeColumn($table, 'hide_results')->type('tinyint')->length(3)->setDefault(0);
-            $this->addOrChangeColumn($table, 'until_close')->type('tinyint')->length(3)->setDefault(0);
-        });
+        $sm = $this->schemaManager();
 
-        \XF::app()->simpleCache()->setValue($this->addOn->getAddOnId(), 'HidePollResults', false);
+        foreach ($this->getAlterTables() as $tableName => $callback)
+        {
+            if ($sm->tableExists($tableName))
+            {
+                $sm->alterTable($tableName, $callback);
+            }
+        }
     }
 
-    public function upgrade2000200Step1()
+    public function installStep2()
     {
-        $this->installStep1();
+        \XF::app()->simpleCache()->setValue($this->addOn->getAddOnId(), 'HidePollResults', false);
     }
 
     public function upgrade2010000Step1()
     {
+        $this->installStep1();
+    }
+
+    public function upgrade2010000Step2()
+    {
+        $this->installStep2();
+    }
+
+    public function upgrade2010000Step3()
+    {
         $this->renamePhrases([
             'the_results_of_this_poll_are_hidden_until_manual' => 'svHidePoll_poll_results_hidden_until_manual_action',
-            'the_results_of_this_poll_are_hidden_until_x' => 'svHidePoll_poll_results_hidden_until_x',
-            'hide_poll_results' => 'svHidePoll_hide_poll_results',
-            'hide_until_close' => 'svHidePoll_hide_until_close',
+            'the_results_of_this_poll_are_hidden_until_x'      => 'svHidePoll_poll_results_hidden_until_x',
+            'hide_poll_results'                                => 'svHidePoll_hide_poll_results',
+            'hide_until_close'                                 => 'svHidePoll_hide_until_close',
             'results_will_be_hidden_until_poll_closes_explain' => 'svHidePoll_hide_until_close_explain',
         ]);
     }
 
+    public function upgrade2010000Step4()
+    {
+        /** @var \XF\Entity\Template $template */
+        $template = \XF::finder('XF:Template')
+                       ->where('title', 'thread_view_hidden_poll_results')
+                       ->where('style_id', 0)
+                       ->fetchOne();
+        if ($template)
+        {
+            $template->title = 'svHidePoll_poll_macros_hide_results';
+            $template->save();
+        }
+    }
+
     public function uninstallStep1()
     {
-        $this->schemaManager()->alterTable('xf_poll', function (Alter $table) {
-            $table->dropColumns(['hide_results', 'until_close']);
-        });
+        $sm = $this->schemaManager();
+
+        foreach ($this->getRemoveAlterTables() as $tableName => $callback)
+        {
+            if ($sm->tableExists($tableName))
+            {
+                $sm->alterTable($tableName, $callback);
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAlterTables()
+    {
+        $tables = [];
+
+        $tables['xf_poll'] = function (Alter $table) {
+            $this->addOrChangeColumn($table, 'hide_results')->type('tinyint')->length(1)->setDefault(0);
+            $this->addOrChangeColumn($table, 'until_close')->type('tinyint')->length(1)->setDefault(0);
+        };
+
+        return $tables;
+    }
+
+    protected function getRemoveAlterTables()
+    {
+        $tables = [];
+
+        $tables['xf_poll'] = function (Alter $table) {
+            $table->dropColumns(['\'hide_results\', \'until_close\'']);
+        };
+
+        return $tables;
     }
 }
